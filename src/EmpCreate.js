36548a0b538b00217af7bce41390aadfd0d99a32
Stanley <cslafond@gmail.com>
2023-02-14 11:43:54 -0500

    Employee Detail

--- src/EmpCreate.js
+++ src/EmpCreate.js
@@ -3,58 +3,116 @@ import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const EmpCreate = () => {
+  const [id, idchange] = useState("");
+  const [name, namechange] = useState("");
+  const [email, emailchange] = useState("");
+  const [phone, phonechange] = useState("");
+  const [active, activechange] = useState(true);
+  const [validation, valchange] = useState(false);
+  
+  const navigate = useNavigate();
+
+  const handlesubmit=(e)=> {
+    e.preventDefault();
+     const empdata = { name, email, phone, active } ;
+
+    fetch("http://localhost:8000/employee", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body:JSON.stringify(empdata),
+    })
+      .then((res) => {
+        alert("Saved Successfully");
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
   return (
     <div>
       <div className="row">
         <div className="offset-lg-3 col-lg-6">
-          <div className="container">
-            <div className="card row" style={{"textAlign":"left"}}>
+          <form className="container" onSubmit={handlesubmit}>
+            <div className="card row" style={{ textAlign: "left" }}>
               <div className="card-title">
                 <h2>Create Employee</h2>
               </div>
               <div className="card-body">
                 <div className="row">
-                  
                   <div className="col-lg-12">
                     <div className="form-group">
                       <label>ID</label>
-                      <input className="form-control"></input>
+                      <input
+                        value={id}
+                        disabled="disabled"
+                        className="form-control"
+                      ></input>
                     </div>
                   </div>
-                  
+
                   <div className="col-lg-12">
                     <div className="form-group">
                       <label>Name</label>
-                      <input className="form-control"></input>
+                      <input
+                        required
+                        value={name}
+                        onMouseDown={e=>valchange(true)}
+                        onChange={e=>namechange(e.target.value)}
+                        className="form-control"
+                      ></input>
+                      {name.length===0 && validation && <span className="text-danger">Enter a name!</span>}
                     </div>
                   </div>
 
                   <div className="col-lg-12">
                     <div className="form-group">
                       <label>Email</label>
-                      <input className="form-control"></input>
+                      <input
+                        value={email}
+                        onChange={(e)=>emailchange(e.target.value)}
+                        className="form-control"
+                      ></input>
                     </div>
                   </div>
 
                   <div className="col-lg-12">
                     <div className="form-group">
                       <label>Phone</label>
-                      <input className="form-control"></input>
+                      <input
+                        value={phone}
+                        onChange={(e)=>phonechange(e.target.value)}
+                        className="form-control"
+                      ></input>
                     </div>
                   </div>
 
                   <div className="col-lg-12">
                     <div className="form-check">
-                    <input type="checkbox" className="form-check-input"></input>
+                      <input
+                        checked={active}
+                        onChange={(e)=>activechange(e.target.checked)}
+                        type="checkbox"
+                        className="form-check-input"
+                      ></input>
                       <label className="form-check-label">Is Active</label>
-                      
                     </div>
                   </div>
-                  
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <button className="btn btn-success" type="submit">
+                        Save
+                      </button>
+                      <Link to="/" className="btn btn-danger">
+                        Back
+                      </Link>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
--- src/EmpDetail.js
+++ src/EmpDetail.js
@@ -3,9 +3,29 @@ import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 const EmpDetail = () => {
-    return (
-        <div></div>
-      );
-}
- 
+  const { empid } = useParams();
+  const [empdata, empdatachange] = useState({});
+  useEffect(() => {
+    fetch("http://localhost:8000/employee/"+empid)
+      .then((res, req) => {
+        return res.json();
+      })
+      .then((resp) => {
+        empdatachange(resp);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+  return (
+    <div>
+      {empdata && 
+        <h1>
+          The employee name is: {empdata.name} ({empdata.id})
+        </h1>
+      }
+    </div>
+  );
+};
+
 export default EmpDetail;
